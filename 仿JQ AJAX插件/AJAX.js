a1871fc5b0530459261b6e7e1871c243c419d7be
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-09-16 15:50:35 +0800

    AJAX封装插件v0.1.3 就改默认为get请求，改函数名AJAX为ajax

--- 仿JQ AJAX插件/AJAX.js
+++ 仿JQ AJAX插件/AJAX.js
@@ -1,30 +1,30 @@
-function AJAX(obj){
-				var reqURL=obj.reqURL;
-				var reqMethod=obj.reqMethod||"post";
-				var reqAsync=obj.reqAsync||true;
-				var reqData=obj.reqData||"";//直接传数据不解析
-				var reqHeader=obj.reqHeader;
-				var reqSuccess=obj.reqSuccess;
-				var reqError=obj.reqError;
-				var reqBefore=obj.reqBefore;
-				
-				var req=new XMLHttpRequest();
-				req.onreadystatechange=function(){
-					if(req.readyState==4)
-					{
-						if(req.status==200){
-							if(reqSuccess)reqSuccess(req.responseText);
-						}else{
-							if(reqError)reqError(req.responseText);
-						}
-					}
-				};
-				req.open(reqMethod,reqURL,reqAsync);
-				if(reqHeader){
-					for(var head in reqHeader){
-						req.setRequestHeader(head,reqHeader[head]);
-					}
-				}
-				if(reqBefore)reqBefore();
-				req.send(reqData);
+function ajax(obj){
+	var reqURL=obj.reqURL;
+	var reqMethod=obj.reqMethod||"get";
+	var reqAsync=obj.reqAsync||true;
+	var reqData=obj.reqData||"";//直接传数据不解析
+	var reqHeader=obj.reqHeader;
+	var reqSuccess=obj.reqSuccess;
+	var reqError=obj.reqError;
+	var reqBefore=obj.reqBefore;
+
+	var req=new XMLHttpRequest();
+	req.onreadystatechange=function(){
+		if(req.readyState==4)
+		{
+			if(req.status==200){
+				if(reqSuccess)reqSuccess(req.responseText);
+			}else{
+				if(reqError)reqError(req.responseText);
 			}
+		}
+	};
+	req.open(reqMethod,reqURL,reqAsync);
+	if(reqHeader){
+		for(var head in reqHeader){
+			req.setRequestHeader(head,reqHeader[head]);
+		}
+	}
+	if(reqBefore)reqBefore();
+	req.send(reqData);
+}
