99ea0b590c52f5c3b1b58b63c1c37e727433c6c7
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-10-10 23:02:04 +0800

    webpackstudy

---
+++ webpackStudy/webpack.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  devtool: 'eval-source-map',
+
+  entry:  __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js"
+  },
+
+  module: {//在配置文件里添加JSON loader
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    colors: true,//终端中输出结果为彩色
+    historyApiFallback: true,//不跳转
+    inline: true,//实时刷新
+    port:3000
+  }
+};
