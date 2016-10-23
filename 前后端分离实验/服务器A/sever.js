16cf9efefe6153b3ae10b66e88b4e9447faff9c7
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-10-23 11:21:28 +0800

    完成HTTPS的实现

--- 前后端分离实验/服务器A/sever.js
+++ 前后端分离实验/服务器A/sever.js
@@ -1,9 +1,18 @@
 var express=require('express'),
+  fs=require('fs'),
+  http=require('http'),
+  https=require('https'),
   path=require('path'),
   bodyParser=require('body-parser'),
   cookieParser=require('cookie-parser'),
   session=require('express-session'),
   request = require('request');
+var privateKey  = fs.readFileSync(__dirname+'/ssl/private.pem', 'utf8');
+var certificate = fs.readFileSync(__dirname+'/ssl/file.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+
+
 
 app=express();
 
@@ -31,10 +40,19 @@ app.use(function(req, res, next){
   next();
 });
 
+
+app.get('/httpstest',function(req,res,next){
+  if(req.protocol === 'https') {
+      res.status(200).send('Welcome to Safety Land!');
+  }
+  else {
+      res.status(200).send('Welcome!');
+  }
+});
+
+
 app.get("/text1",function(req,res,next){
   request("http://www.cxyblogbiu.com/ajax/articles/1", function (error, response, body){
-
-    
     res.contentType('JSON');
     res.send(body);
     res.end();
@@ -42,7 +60,13 @@ app.get("/text1",function(req,res,next){
 });
 
 
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-app.listen(3000,function(){
+httpServer.listen(3000,function(){
 	console.log(' - listening on http://*:'+3000);
+});
+
+httpsServer.listen(3001,function(){
+	console.log(' - listening on http://*:'+3001);
 });
