947eccdc23070c811f2504c9844926ed31ccf71d
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-12-24 18:57:47 +0800

    完善工程

--- vueStudy/vue-router/webpack.config.js
+++ vueStudy/vue-router/webpack.config.js
@@ -3,37 +3,74 @@ var path = require('path');
 var webpack = require('webpack');
 // 模块导入
 module.exports = {
-    devtool: 'source-map-eval',
     entry: __dirname+'/res/index.js',//入口
     output: {
         path: path.join(__dirname, './build'),
+        publicPath: "/build/",
         filename: 'bundle.js',
     },
-    resolveLoader: {
-      root: path.join(__dirname, 'node_modules'),
-    },
     module: {
-        loaders: [
-          {
-            test:/\.vue$/,
-            loader:'vue'
-          },
-          {
-            test: /\.js$/,
-            loader: 'babel',
-            exclude: /node_modules/
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this nessessary.
+            'scss': 'vue-style-loader!css-loader!sass-loader',
+            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-        ]
-    },
-    resolve: {
-      alias: {
-        'vue$': 'vue/dist/vue.js'
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
       }
-    },
-    plugins: [
-     new webpack.DllReferencePlugin({
-       context: __dirname,
-       manifest: require('./manifest.json'),
-     })
-   ]
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  devServer: {
+    contentBase: "build/",
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
 };
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
