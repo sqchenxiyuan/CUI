d6f204738e78373dd7daa6927c1b6cdd2f9cfc24
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2016-12-28 20:49:37 +0800

    完成对hash的解析

--- ScrollTool/src/index.js
+++ ScrollTool/src/index.js
@@ -1,59 +1,90 @@
-document.ready=function(fun){
-    var t = setInterval(a,10);//设计一个定时器，循环调用a函数
-    function a(){
-        if(document && document.getElementsByTagName && document.getElementById && document.body)
-        {
-            fun();
-            clearInterval(t);
+(function(){
+  window.easyScroll=function(){
+    var easyScroll={};
+    var label='data-scroll';
+    var hashStart='scrollS=';
+    var hashEnd='=scrollE';
+    var reg=null;
+
+    easyScroll.init=function(){
+      reg=new RegExp(hashStart+'(\\S+)'+hashEnd,'g');
+      var items=Array.from(document.querySelectorAll(`[${label}]`));
+      items.forEach(function(item){
+        item.onclick=function(){return false;};
+        item.removeEventListener('click',scrollEvent);
+        item.addEventListener('click',scrollEvent);
+      });
+      initScroll();
+    };
+
+    function initScroll(){
+      var id=null;
+      if((id=URL2ID())){
+        scrollTo(id);
+      }
+    }
+
+
+    function scrollEvent(){
+      var to=null;
+      if(!(to=this.getAttribute(label))){
+        to=this.getAttribute('href');
+      }
+      ID2URL(to);
+      scrollTo(to);
+    }
+
+    function scrollTo(to){
+      var todom=document.querySelector(to);
+      setScroll(document.body,todom.offsetTop,500);
+    }
+
+    function setScroll(dom,to,speed){
+      var starttime=new Date().getTime();
+      var starttop=dom.scrollTop;
+      var delay=Math.abs(starttop-to)/(speed*window.screen.availHeight/100)*1000;
+
+      startScroll();
+
+      function startScroll(){
+        if(change()){
+          setTimeout(startScroll,15);
         }
+      }
+
+      function change(){
+        var nowtime=new Date().getTime();
+        var p=(nowtime-starttime)/delay;
+        if(p>1)p=1;
+        dom.scrollTop=starttop+(to-starttop)*p;
+
+        if(p===1){
+          return false;
+        }else{
+          return true;
+        }
+      }
     }
-};
-
-document.ready(function(){
-  var items=Array.from(document.querySelectorAll('[data-scroll]'));
-  items.forEach(function(item){
-    item.onclick=function(){return false;};
-    item.addEventListener('click',scrollEvent);
-  });
-});
 
 
-function scrollEvent(){
-  var to=this.getAttribute('href').replace(/^#/,'');
-
-  var todom=document.getElementById(to);
-  scrollTo(todom);
-}
-
-function scrollTo(dom){
-  console.log(dom.offsetTop);
-  console.log(document.body.scrollTop);
-  setScroll(document.body,dom.offsetTop,500);
-}
-
-function setScroll(dom,to,speed){
-  var starttime=new Date().getTime();
-  var starttop=dom.scrollTop;
-  var delay=Math.abs(starttop-to)/(speed*window.screen.availHeight/100)*1000;
-
-  startScroll();
-
-  function startScroll(){
-    if(change()){
-      setTimeout(startScroll,10);
-    }
-  }
-
-  function change(){
-    var nowtime=new Date().getTime();
-    var p=(nowtime-starttime)/delay;
-    if(p>1)p=1;
-    dom.scrollTop=starttop+(to-starttop)*p;
-
-    if(p===1){
+    function URL2ID(){
+      var hash=location.hash;
+      if(hash.match(reg)[0]){
+        return hash.match(reg)[0].replace(hashStart,'').replace(hashEnd,'');
+      }
       return false;
-    }else{
-      return true;
     }
-  }
-}
+
+    function ID2URL(id){
+      var hash=location.hash;
+      if(reg.test(hash)){
+        hash=hash.replace(reg,`${hashStart}${id}${hashEnd}`);
+      }else{
+        hash+=`${hashStart}${id}${hashEnd}`;
+      }
+      location.hash=hash;
+    }
+
+    return easyScroll;
+  }();
+}());
