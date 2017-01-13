93f171af36589eb33e4f795c88e541e93067938a
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2017-01-13 13:27:25 +0800

    更新AJAX插件

--- 仿JQ AJAX插件/AJAX.js
+++ 仿JQ AJAX插件/AJAX.js
@@ -1,7 +1,18 @@
+/*
+  obj选项：
+  必选： reqURL:请求地址
+  可选： reqMethod:请求方法  默认:GET
+        reqAsync:请求异同步 默认:异步
+        reqData:请求数据    默认:空
+        reqHeader:请求头    默认为空   传入JS对象即可
+        reqSuccess(data):请求成功时间函数   传入返回的数据
+        reqError():请求失败时的响应函数
+        reqBefore:发送请求前的函数
+*/
 function ajax(obj){
 	var reqURL=obj.reqURL;
 	var reqMethod=obj.reqMethod||"get";
-	var reqAsync=obj.reqAsync||true;
+	var reqAsync=(obj.reqAsync===undefined?true:obj.reqAsync);
 	var reqData=obj.reqData||"";//直接传数据不解析
 	var reqHeader=obj.reqHeader;
 	var reqSuccess=obj.reqSuccess;
@@ -15,9 +26,9 @@ function ajax(obj){
 		if(req.readyState==4)
 		{
 			if(req.status==200){
-				if(reqSuccess)reqSuccess(req.responseText);
+				if(reqSuccess&&typeof reqSuccess === 'function')reqSuccess(req.responseText);
 			}else{
-				if(reqError)reqError(req.responseText);
+				if(reqError&&typeof reqError === 'function')reqError(req.responseText);
 			}
 		}
 	};
@@ -27,14 +38,30 @@ function ajax(obj){
 			req.setRequestHeader(head,reqHeader[head]);
 		}
 	}
-	if(reqBefore)reqBefore(req);
+	if(reqBefore&&typeof reqBefore === 'function')reqBefore(req);
 	req.send(reqData);
 }
 
-function get(url,data,success){
-	ajax({
-		reqURL:url,
-		reqData:JSON.stringify(data),
-		reqSuccess:success
-	});
+function get(url,data,success,error){
+  var arr=[];
+  for(var name in data){
+    arr.push(name+"="+data[name]);
+  }
+  url=url+'?'+arr.join('&');
+
+  ajax({
+    reqURL:url,
+    reqSuccess:success,
+    reqError:error
+  });
+}
+
+function post(url,data,success,error){
+  ajax({
+    reqURL:url,
+    reqMethod:'post',
+    reqData:JSON.stringify(data),
+    reqSuccess:success,
+    reqError:error
+  });
 }
