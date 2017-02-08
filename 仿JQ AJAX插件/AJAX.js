b7bcf53a2f85ff5eb68db3b220e73d9a04851d32
sqchenxiyuan <s.q.chenxiyuan@gmail.com>
2017-02-08 09:36:36 +0800

    完善AJAX插件

--- 仿JQ AJAX插件/AJAX.js
+++ 仿JQ AJAX插件/AJAX.js
@@ -5,9 +5,13 @@
         reqAsync:请求异同步 默认:异步
         reqData:请求数据    默认:空
         reqHeader:请求头    默认为空   传入JS对象即可
+				reqUserName:请求用户名	默认为空
+				reqUserPassWord:请求密码	默认为空
         reqSuccess(data):请求成功时间函数   传入返回的数据
-        reqError():请求失败时的响应函数
+        reqError(data):请求失败时的响应函数
         reqBefore:发送请求前的函数
+				reqProgress:请求获取的进度
+				reqUploadProgress:请求发出的进度
 */
 function ajax(obj){
 	var reqURL=obj.reqURL;
@@ -15,11 +19,14 @@ function ajax(obj){
 	var reqAsync=(obj.reqAsync===undefined?true:obj.reqAsync);
 	var reqData=obj.reqData||"";//直接传数据不解析
 	var reqHeader=obj.reqHeader;
+	var reqUserName=obj.reqUserName||"";
+	var reqUserPassWord=obj.reqUserPassWord||"";
+
 	var reqSuccess=obj.reqSuccess;
 	var reqError=obj.reqError;
 	var reqBefore=obj.reqBefore;
-	var reqUserName=obj.reqUserName||"";
-	var reqUserPassWord=obj.reqUserPassWord||"";
+	var reqProgress=obj.reqProgress;
+	var reqUploadProgress=obj.reqUploadProgress;
 
 	var req=new XMLHttpRequest();
 	req.onreadystatechange=function(){
@@ -32,6 +39,10 @@ function ajax(obj){
 			}
 		}
 	};
+	req.onprogress=reqProgress;
+	req.upload.onprogress=reqUploadProgress;
+
+
 	req.open(reqMethod,reqURL,reqAsync,reqUserName,reqUserPassWord);
 	if(reqHeader){
 		for(var head in reqHeader){
@@ -47,7 +58,7 @@ function get(url,data,success,error){
   for(var name in data){
     arr.push(name+"="+data[name]);
   }
-  url=url+'?'+arr.join('&');
+  if(arr.length>0)url=url+'?'+arr.join('&');
 
   ajax({
     reqURL:url,
@@ -56,11 +67,17 @@ function get(url,data,success,error){
   });
 }
 
+function getJSON(url,data,success,error){
+	get(url,data,function(data){
+		success(JSON.parse(data));
+	},error);
+}
+
 function post(url,data,success,error){
   ajax({
     reqURL:url,
     reqMethod:'post',
-    reqData:JSON.stringify(data),
+    reqData:data,
     reqSuccess:success,
     reqError:error
   });
